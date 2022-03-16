985303c3cec919dad4dc03be9510f599da65b853
zhaohaihzb@126.com <zhaohaihzb@126.com>
2022-03-16 15:55:11 +0800

    add test

--- connectors/aliyun/rocketmq-connect-http/src/test/java/org/apache/rocketmq/connect/http/sink/HttpSinkConnectorTest.java
+++ connectors/aliyun/rocketmq-connect-http/src/test/java/org/apache/rocketmq/connect/http/sink/HttpSinkConnectorTest.java
@@ -1,7 +1,14 @@
 package org.apache.rocketmq.connect.http.sink;
 
+import io.openmessaging.KeyValue;
+import io.openmessaging.connector.api.data.ConnectRecord;
+import io.openmessaging.internal.DefaultKeyValue;
+import org.apache.rocketmq.connect.http.sink.constant.HttpConstant;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HttpSinkConnectorTest {
 
@@ -12,4 +19,16 @@ public class HttpSinkConnectorTest {
         Assert.assertEquals(httpSinkConnector.taskConfigs(1).size(), 1);
     }
 
+    @Test
+    public void testPut() {
+        HttpSinkTask httpSinkTask = new HttpSinkTask();
+        KeyValue keyValue = new DefaultKeyValue();
+        keyValue.put(HttpConstant.URL_CONSTANT, "http://127.0.0.1:8081/demo");
+        httpSinkTask.init(keyValue);
+        List<ConnectRecord> connectRecordList = new ArrayList<>();
+        ConnectRecord connectRecord = new ConnectRecord(null ,null, System.currentTimeMillis());
+        connectRecord.setData("test");
+        connectRecordList.add(connectRecord);
+        httpSinkTask.put(connectRecordList);
+    }
 }
