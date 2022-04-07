f79cd4c29d794ca5c76a16d24cbec56d979b4f42
doubleDimple <lovele.cn@gmail.com>
2022-04-07 18:20:18 +0800

    RedisSinkTask commit

--- connectors/rocketmq-connect-redis/src/main/java/org/apache/rocketmq/connect/redis/connector/RedisSinkTask.java
+++ connectors/rocketmq-connect-redis/src/main/java/org/apache/rocketmq/connect/redis/connector/RedisSinkTask.java
@@ -20,12 +20,22 @@ package org.apache.rocketmq.connect.redis.connector;
 import com.alibaba.fastjson.JSONObject;
 import io.openmessaging.KeyValue;
 import io.openmessaging.connector.api.common.QueueMetaData;
+import io.openmessaging.connector.api.data.EntryType;
 import io.openmessaging.connector.api.data.Field;
 import io.openmessaging.connector.api.data.Schema;
 import io.openmessaging.connector.api.data.SinkDataEntry;
 import io.openmessaging.connector.api.sink.SinkTask;
+import org.apache.rocketmq.connect.redis.config.Config;
+import org.apache.rocketmq.connect.redis.converter.KVEntryConverter;
+import org.apache.rocketmq.connect.redis.converter.RedisEntryConverter;
+import org.apache.rocketmq.connect.redis.handler.DefaultRedisEventHandler;
+import org.apache.rocketmq.connect.redis.handler.RedisEventHandler;
+import org.apache.rocketmq.connect.redis.processor.DefaultRedisEventProcessor;
+import org.apache.rocketmq.connect.redis.processor.RedisEventProcessor;
+import org.apache.rocketmq.connect.redis.sink.RedisUpdater;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -38,15 +48,40 @@ public class RedisSinkTask extends SinkTask {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisSinkTask.class);
 
+    private RedisUpdater updater;
+
+    /**
+     * listening and handle Redis event.
+     */
+    private RedisEventProcessor eventProcessor;
+    private Config config;
+    /**
+     * convert kVEntry to list of sourceDataEntry
+     */
+    private KVEntryConverter kvEntryConverter;
+
+    public RedisEventProcessor getEventProcessor() {
+        return eventProcessor;
+    }
+
+    public void setEventProcessor(RedisEventProcessor eventProcessor) {
+        this.eventProcessor = eventProcessor;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
     @Override
     public void put(Collection<SinkDataEntry> sinkDataEntries) {
              //save data from MQ to redis
-            for (SinkDataEntry record : sinkDataEntries) {
+            for (SinkDataEntry sinkDataEntry : sinkDataEntries) {
                 Map<Field, Object[]> fieldMap = new HashMap<>();
-                Object[] payloads = record.getPayload();
-                Schema schema = record.getSchema();
-                //判断schema的datasource,是不是redis,如果是redis,按照redis命令存储,
-                //如果不是redis,则直接将数据存储到redis的集合中
+                Object[] payloads = sinkDataEntry.getPayload();
+
+                Schema schema = sinkDataEntry.getSchema();
+                EntryType entryType = sinkDataEntry.getEntryType();
+
                 List<Field> fields = schema.getFields();
                 Boolean parseError = false;
                 if (!fields.isEmpty()) {
@@ -62,7 +97,10 @@ public class RedisSinkTask extends SinkTask {
                     }
                 }
                 if (!parseError) {
-
+                    Boolean isSuccess = updater.push(fieldMap, entryType);
+                    if (!isSuccess) {
+                        LOGGER.error("push data error, entryType:{}, fieldMap:{}", fieldMap, entryType);
+                    }
                 }
             }
     }
@@ -73,13 +111,36 @@ public class RedisSinkTask extends SinkTask {
     }
 
     @Override
-    public void start(KeyValue config) {
+    public void start(KeyValue keyValue) {
+        this.kvEntryConverter = new RedisEntryConverter();
 
+        this.config = new Config();
+        this.config.load(keyValue);
+        LOGGER.info("task config msg: {}", this.config.toString());
+
+        this.eventProcessor = new DefaultRedisEventProcessor(config);
+        RedisEventHandler eventHandler = new DefaultRedisEventHandler(this.config);
+        this.eventProcessor.registEventHandler(eventHandler);
+        try {
+            this.eventProcessor.start();
+            LOGGER.info("Redis task start.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error("processor start error: [{}]", e.getMessage());
+            this.stop();
+        }
     }
 
     @Override
     public void stop() {
-
+        if (this.eventProcessor != null) {
+            try {
+                this.eventProcessor.stop();
+                LOGGER.info("Redis task is stopped.");
+            } catch (IOException e) {
+                LOGGER.error("processor stop error: {}", e);
+            }
+        }
     }
 
     @Override
