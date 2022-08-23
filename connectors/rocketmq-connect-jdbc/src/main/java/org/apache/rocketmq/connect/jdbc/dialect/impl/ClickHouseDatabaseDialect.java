01833e4981437cf7031107ac7adcaedec8966938
James <1849483422@qq.com>
2022-08-23 08:31:46 +0800

    update

---
+++ connectors/rocketmq-connect-jdbc/src/main/java/org/apache/rocketmq/connect/jdbc/dialect/impl/ClickHouseDatabaseDialect.java
@@ -0,0 +1,90 @@
+package org.apache.rocketmq.connect.jdbc.dialect.impl;
+import org.apache.rocketmq.connect.jdbc.config.AbstractConfig;
+import org.apache.rocketmq.connect.jdbc.dialect.DatabaseDialect;
+import org.apache.rocketmq.connect.jdbc.dialect.provider.DatabaseDialectProvider;
+import org.apache.rocketmq.connect.jdbc.schema.table.TableId;
+import org.apache.rocketmq.connect.jdbc.sink.metadata.SinkRecordField;
+import org.apache.rocketmq.connect.jdbc.util.IdentifierRules;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.apache.rocketmq.connect.jdbc.schema.column.ColumnId;
+import java.util.Collection;
+
+    /**
+     *  ClickHouse Dialect
+     */
+public class ClickHouseDatabaseDialect extends GenericDatabaseDialect {
+
+        private final static Logger log = LoggerFactory.getLogger(ClickHouseDatabaseDialect.class);
+
+        /**
+         * The provider for {@link ClickHouseDatabaseDialect}.
+         */
+
+        public static class Provider extends DatabaseDialectProvider {
+            public Provider() throws ClassNotFoundException {
+                super(ClickHouseDatabaseDialect.class.getSimpleName(), "clickhouse");
+                Class.forName("com.clickhouse.jdbc.ClickHouseDriver");
+            }
+
+            @Override
+            public DatabaseDialect create(AbstractConfig config) {
+                return new ClickHouseDatabaseDialect(config);
+            }
+        }
+
+        public ClickHouseDatabaseDialect(AbstractConfig config) {
+            super(config, new IdentifierRules(".", "`", "`"));
+        }
+
+        @Override
+        protected void initializePreparedStatement(PreparedStatement stmt) throws SQLException {
+            stmt.setFetchSize(Integer.MIN_VALUE);
+            log.trace("Initializing PreparedStatement fetch direction to FETCH_FORWARD for '{}'", stmt);
+            stmt.setFetchDirection(ResultSet.FETCH_FORWARD);
+        }
+
+
+        // 在这里作clickhouse的数据字段类型转换
+        @Override
+        protected String getSqlType(SinkRecordField field) {
+            switch (field.schemaType()) {
+                case STRING:
+                    return "String";
+                case INT8:
+                    return "Int8";
+                case INT16:
+                    return "Int16";
+                case INT32:
+                    return "Int32";
+                case INT64:
+                    return "Int64";
+                case FLOAT32:
+                    return "Float32";
+                case FLOAT64:
+                    return "Float64";
+                default:
+                    return super.getSqlType(field);
+            }
+        }
+
+        @Override
+        public String buildUpsertQueryStatement(TableId table,
+                                                Collection<ColumnId> keyColumns,
+                                                Collection<ColumnId> nonKeyColumns) {
+            return super.buildInsertStatement(table, keyColumns, nonKeyColumns);
+        }
+
+
+
+        @Override
+        public String buildInsertStatement(TableId table,
+                                           Collection<ColumnId> keyColumns,
+                                           Collection<ColumnId> nonKeyColumns) {
+            return super.buildInsertStatement(table, keyColumns, nonKeyColumns);
+        }
+
+}
---
+++ connectors/rocketmq-connect-jdbc/src/test/java/org/apache/rocketmq/connect/jdbc/connector/sink/ClickHouseJdbcSinkTest.java
@@ -0,0 +1,82 @@
+package org.apache.rocketmq.connect.jdbc.connector.sink;
+
+import io.openmessaging.KeyValue;
+import io.openmessaging.connector.api.data.*;
+import io.openmessaging.internal.DefaultKeyValue;
+import org.apache.rocketmq.connect.jdbc.connector.JdbcSinkTask;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class ClickHouseJdbcSinkTest {
+
+    private static JdbcSinkTask CKHouseSinkTask = new JdbcSinkTask();
+    private static final String dbName = "default";
+    private static final String tableName = "tb_test";
+    private static final String ip = "47.118.70.66";
+    private static final String port = "18123";
+    @Before
+    public void testCKHouseSinkConfig() {
+        KeyValue config = buildClickHouseSinkConfig();
+        CKHouseSinkTask.start(config);
+    }
+
+    @After
+    public void close() {}
+
+
+    private KeyValue buildClickHouseSinkConfig() {
+        KeyValue conf = new DefaultKeyValue();
+        conf.put("connector-class", "org.apache.rocketmq.connect.jdbc.connector.JdbcSinkConnector");
+        conf.put("max-task", "1");
+        conf.put("connect-topicname", "tb_test");
+        conf.put("connection.url",String.format("jdbc:clickhouse://%s:%s",ip,port));
+        conf.put("connection.user", "");
+        conf.put("connection.password","");
+        conf.put("pk.fields", "id");
+        conf.put("pk.mode" , "record_value");
+        conf.put("insert.mode" ,"UPSERT");
+        conf.put("source-record-converter", "org.apache.rocketmq.connect.runtime.converter.record.json.JsonConverter");
+        return conf;
+    }
+
+    /**
+     * sink writer test
+     */
+    @Test
+    public void testOpenMLDBJdbcSinkWriterTest() throws SQLException {
+        List<ConnectRecord> records = new ArrayList<>();
+
+        // build schema
+        Schema schema = SchemaBuilder.struct()
+                .name(tableName)
+                .field("id",SchemaBuilder.int32().build())
+                .field("name", SchemaBuilder.string().build())
+                .build();
+
+        // build record
+        int id = 1;
+        String name = "开源";
+        io.openmessaging.connector.api.data.Struct struct= new Struct(schema);
+        struct.put("id",id);
+        struct.put("name",name);
+
+        ConnectRecord record = new ConnectRecord(
+                // offset partition
+                // offset partition"
+                new RecordPartition(new ConcurrentHashMap<>()),
+                new RecordOffset(new HashMap<>()),
+                System.currentTimeMillis(),
+                schema,
+                struct
+        );
+        records.add(record);
+        CKHouseSinkTask.put(records);
+    }
+}
