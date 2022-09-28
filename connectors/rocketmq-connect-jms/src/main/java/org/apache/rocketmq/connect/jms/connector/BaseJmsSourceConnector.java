da54abb8f41a15627cb1d6cafdc1c11a0f98380d
Oliver <wqdyxnbd@163.com>
2022-09-28 14:15:54 +0800

    [ISSUE #292] Jms source connector adapt to new api (#293)
    * [ISSUE #292] Jms source connector adapt to new api
    
    * set position and optimize connectrecord

--- connectors/rocketmq-connect-jms/src/main/java/org/apache/rocketmq/connect/jms/connector/BaseJmsSourceConnector.java
+++ connectors/rocketmq-connect-jms/src/main/java/org/apache/rocketmq/connect/jms/connector/BaseJmsSourceConnector.java
@@ -17,51 +17,38 @@
 
 package org.apache.rocketmq.connect.jms.connector;
 
+import io.openmessaging.connector.api.component.task.Task;
+import io.openmessaging.connector.api.component.task.source.SourceConnector;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import io.openmessaging.KeyValue;
-import io.openmessaging.connector.api.Task;
-import io.openmessaging.connector.api.source.SourceConnector;
 
 public abstract class BaseJmsSourceConnector extends SourceConnector {
 
     private KeyValue config;
 
     @Override
-    public String verifyAndSetConfig(KeyValue config) {
+    public void start(KeyValue config) {
         for (String requestKey : getRequiredConfig()) {
             if (!config.containsKey(requestKey)) {
-                return "Request config key: " + requestKey;
+                throw new RuntimeException("Request config key: " + requestKey);
             }
         }
         this.config = config;
-        return "";
-    }
-
-    @Override
-    public void start() {
-
     }
 
     @Override
     public void stop() {
-
+        this.config = null;
     }
 
-    @Override public void pause() {
-
-    }
-
-    @Override public void resume() {
-
-    }
 
     @Override
     public abstract Class<? extends Task> taskClass();
 
     @Override
-    public List<KeyValue> taskConfigs() {
+    public List<KeyValue> taskConfigs(int maxTask) {
         List<KeyValue> config = new ArrayList<>();
         config.add(this.config);
         return config;
