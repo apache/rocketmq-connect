d689d41435d92187181eb05e33d956e7490ee278
Oliver <wqdyxnbd@163.com>
2022-11-18 21:33:50 +0800

    feat:add checkIndex and checkMapping

--- connectors/rocketmq-connect-elasticsearch/src/main/java/org/apache/rocketmq/connect/elasticsearch/connector/ElasticsearchSinkTask.java
+++ connectors/rocketmq-connect-elasticsearch/src/main/java/org/apache/rocketmq/connect/elasticsearch/connector/ElasticsearchSinkTask.java
@@ -22,10 +22,15 @@ import io.openmessaging.KeyValue;
 import io.openmessaging.connector.api.component.task.sink.SinkTask;
 import io.openmessaging.connector.api.data.ConnectRecord;
 import io.openmessaging.connector.api.data.Field;
+import io.openmessaging.connector.api.data.FieldType;
 import io.openmessaging.connector.api.data.Struct;
 import io.openmessaging.connector.api.errors.ConnectException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -39,6 +44,13 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetMappingsRequest;
+import org.elasticsearch.client.indices.GetMappingsResponse;
+import org.elasticsearch.client.indices.PutMappingRequest;
+import org.elasticsearch.cluster.metadata.MappingMetadata;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,13 +63,23 @@ public class ElasticsearchSinkTask extends SinkTask {
 
     private RestHighLevelClient restHighLevelClient;
 
+    private Set<String> indexCache;
+
+    private Map<String, Boolean> mappingCache;
+
+    private static final String PROPERTIES_FIELD = "properties";
+
+    private static final String TYPE_FIELD = "type";
+
     @Override
     public void put(List<ConnectRecord> sinkRecords) throws ConnectException {
         if (sinkRecords == null || sinkRecords.size() < 1) {
             return;
         }
         for (ConnectRecord record : sinkRecords) {
-            final String indexName = record.getExtension(ElasticsearchConstant.INDEX);
+            String indexName = record.getExtension(ElasticsearchConstant.INDEX);
+            this.checkIndexIsExist(indexName);
+            this.checkMappingIsExist(indexName, record);
             if (indexName == null || indexName == "") {
                 continue;
             }
@@ -80,6 +102,8 @@ public class ElasticsearchSinkTask extends SinkTask {
 
     @Override
     public void start(KeyValue keyValue) {
+        this.indexCache = new ConcurrentSkipListSet<>();
+        this.mappingCache = new ConcurrentHashMap<>();
         this.config = new ElasticsearchConfig();
         this.config.load(keyValue);
         HttpHost httpHost = new HttpHost(config.getElasticsearchHost(), config.getElasticsearchPort());
@@ -99,8 +123,90 @@ public class ElasticsearchSinkTask extends SinkTask {
         try {
             restHighLevelClient.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error("restHighLevelClient close failed", e);
+        }
+    }
+
+    private void checkIndexIsExist(String index) {
+        if (indexCache.contains(index)) {
+            return;
+        }
+        CreateIndexRequest request = new CreateIndexRequest(index);
+        try {
+            restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
+            log.info("create index {} success", index);
+        } catch (IOException e) {
+            log.error("create index failed", e);
+        }
+        indexCache.add(index);
+    }
+
+    private void checkMappingIsExist(String index, ConnectRecord record) {
+        if (mappingCache.containsKey(index)) {
+            return;
+        }
+        GetMappingsRequest request = new GetMappingsRequest().indices(index);
+        try {
+            final GetMappingsResponse response = restHighLevelClient.indices().getMapping(request, RequestOptions.DEFAULT);
+            final MappingMetadata mapping = response.mappings().get(index);
+            if  (mapping != null && mapping.sourceAsMap() != null && !mapping.sourceAsMap().isEmpty()) {
+                return;
+            }
+            // create mapping
+            PutMappingRequest putMappingRequest = new PutMappingRequest(index).source(buildXContentBuilder(record));
+            restHighLevelClient.indices().putMapping(putMappingRequest, RequestOptions.DEFAULT);
+            mappingCache.put(index, true);
+        } catch (IOException e) {
+            log.error("create  mapping failed index: {}", index);
+        }
+    }
+
+    private XContentBuilder buildXContentBuilder(ConnectRecord record) {
+        XContentBuilder builder = null;
+        try {
+            builder = XContentFactory.jsonBuilder();
+            builder = builder.startObject();
+            final List<Field> fields = record.getSchema().getFields();
+            builder = builder.startObject(PROPERTIES_FIELD);
+            for (Field field : fields) {
+                builder = builder.startObject(field.getName());
+                final FieldType type = field.getSchema().getFieldType();
+                switch (type) {
+                    case INT32:
+                        builder = builder.field(TYPE_FIELD, "integer");
+                        break;
+                    case INT64:
+                        builder = builder.field(TYPE_FIELD, "long");
+                        break;
+                    case STRING:
+                        builder = builder.field(TYPE_FIELD, "keyword");
+                        break;
+                    case DATETIME:
+                        builder = builder.field(TYPE_FIELD, "datetime");
+                        break;
+                    case INT8:
+                        builder = builder.field(TYPE_FIELD, "short");
+                        break;
+                    case INT16:
+                        builder = builder.field(TYPE_FIELD, "short");
+                        break;
+                    case FLOAT32:
+                        builder = builder.field(TYPE_FIELD, "float");
+                        break;
+                    case FLOAT64:
+                        builder = builder.field(TYPE_FIELD, "double");
+                        break;
+                    default:
+                        break;
+                }
+                builder = builder.endObject();
+            }
+            builder = builder.endObject();
+            builder = builder.endObject();
+        } catch (IOException e) {
+            log.error("build mapping failed, record: {}", record);
         }
 
+        return builder;
     }
 }
